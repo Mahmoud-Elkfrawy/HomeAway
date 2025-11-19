6c941d3d06723bcf94384c888d616255dff9e115
Mahmoud-Elkfrawy <MahmoudElkfrawydev@gmail.com>
2025-11-19 17:28:27 +0200

    Add domain entities for Hotel, Room, Reservation, and User
    Introduced new domain entities in the `HomeAway.Domain.Entities`
    namespace to model the core application data:
    
    - Added `Hotel` class to represent hotels with properties for
      `Id`, `Name`, `Address`, and associated `Room` objects.
    - Added `Room` class to represent hotel rooms with properties
      for `Id`, `HotelId`, `Number`, `Type`, `Capacity`, and
      availability status. Includes a reference to the `Hotel`.
    - Added `Reservation` class to represent bookings with
      properties for `Id`, `RoomId`, `UserId`, `DateRange`,
      `TotalPrice`, and `Status`. Includes references to `Room`
      and `User`.
    - Added `User` class to represent users with properties for
      `Id`, `FullName`, `UserName`, and `Email`. Password-related
      functionality is excluded for separation of concerns.

---
+++ HomeAway.Domain/Entities/Reservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HomeAway.Domain.Enums;
+using HomeAway.Domain.ValueObjects;
+
+namespace HomeAway.Domain.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public DateRange DateRange { get; set; }
+        public Money TotalPrice { get; set; }
+
+        public ReservationStatus Status { get; set; }
+    }
+}
