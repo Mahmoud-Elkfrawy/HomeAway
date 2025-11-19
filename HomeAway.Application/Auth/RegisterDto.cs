f4409b7e22c1171fbe3098f35aa5e02eb8a7f2eb
Mahmoud-Elkfrawy <MahmoudElkfrawydev@gmail.com>
2025-11-19 22:54:51 +0200

    Add DTOs for Auth; Refactor User; Add Identity package
    Added `LoginDto` and `RegisterDto` in `HomeAway.Application.Auth`
    to support login and registration functionality.
    
    Refactored the `User` class in `HomeAway.Domain.Entities`:
    - Removed `UserName` and `Email` properties.
    - Removed outdated comment about password hash.
    
    Updated `HomeAway.Infrastructure.csproj`:
    - Added `Microsoft.AspNetCore.Identity.EntityFrameworkCore`
      version `8.0.22` for identity-related features.

--- HomeAway.Application/Auth/RegisterDto.cs
+++ HomeAway.Application/Auth/RegisterDto.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeAway.Domain.Entities
+namespace HomeAway.Application.Auth
 {
-    public class User
+    public class RegisterDto
     {
-        public int Id { get; set; }
         public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
