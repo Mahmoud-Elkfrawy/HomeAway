b0d7156df2f173563c47a16dcc0a389666720632
Mahmoud-Elkfrawy <MahmoudElkfrawydev@gmail.com>
2025-11-19 17:27:16 +0200

    Add:Enums To Domain layer

---
+++ HomeAway.Domain/Enums/RoomType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAway.Domain.Enums
+{
+    public enum RoomType
+    {
+        Single,
+        Double,
+        Suite,
+        Deluxe
+    }
+}
