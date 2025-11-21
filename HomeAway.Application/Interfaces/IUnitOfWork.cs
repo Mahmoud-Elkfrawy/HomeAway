db62cc2e5bf1cca727862316c1a674a1283060fa
Mahmoud-Elkfrawy <MahmoudElkfrawydev@gmail.com>
2025-11-21 16:18:43 +0200

    Add foundational structure for repository and unit-of-work
    Introduced the `IUnitOfWork` interface in the `HomeAway.Application.Interfaces` namespace, defining the `CompleteAsync` method for committing changes asynchronously. This interface also includes commented-out repository properties for potential future use.
    
    Added empty, internal classes for `DbSeeder`, `HotelRepository`, `ReservationRepository`, `RoomRepository`, `UserRepository`, and `UnitOfWork` in their respective namespaces within the `HomeAway.Infrastructure` layer. These classes serve as placeholders for implementing the repository and unit-of-work pattern.
    
    These changes establish the groundwork for data access and persistence logic in the `HomeAway` application.

---
+++ HomeAway.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAway.Application.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        //IBookingRepository Bookings { get; }
+        //IRoomRepository Rooms { get; }
+        //IUserRepository Users { get; }
+
+        Task<int> CompleteAsync();
+    }
+}
